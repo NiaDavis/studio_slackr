0e800ab3db89b59748d581a5844e5e9c3787ab41
Nia <niadavislad@gmail.com>
2018-01-08 22:26:40 +0000

    studio slackr project

--- src/App.js
+++ src/App.js
@@ -16,8 +16,11 @@ import Menu, { MenuItem } from 'material-ui/Menu';
 import React from 'react';
 import base from './rebase';
 import firebase from 'firebase/app';
-import logo from './logo.svg';
 import { CircularProgress } from 'material-ui/Progress';
+
+import Header from './Header/';
+
+
 
 // TODO move these css imports to their respective components once created.
 import './AddMessageInput.css';
@@ -34,11 +37,8 @@ export default class App extends React.Component {
     // TODO this state object got massive! It needs to be broken apart along
     // with the subcomponents
     this.state = {
-      open: false,
-      user: {},
       channels: {},
       loading: true,
-      status: 'loading',
       newChannelName: '',
       newMessage: '',
     };
@@ -68,31 +68,31 @@ export default class App extends React.Component {
   setSelectedChannel(channel) {
     this.setState({selected: channel});
   }
-  render() {
-    // TODO this code to generate the header should be moved a component.
-    var userStatus = <CircularProgress/>; // For loading state
-    if(this.state.status === 'in') {
-      userStatus = (<div onClick={this.handleOpen.bind(this)}>
-        <img alt="user profile"
-          className="Header-photo" src={this.state.user.photoURL} />
-        <Menu anchorEl={this.state.anchorEl} open={this.state.open}
-          onClose={this.handleClose.bind(this)}>
-          <MenuItem onClick={this.handleSignOut}>Sign out</MenuItem>
-        </Menu>
-      </div>);
-    } else if(this.state.status === 'out') {
-      userStatus = (<Button raised color="primary" onClick={this.handleSignIn}>
-        SIGN IN
-      </Button>);
-    }
-    var Header = (
-        <header className="Header">
-          <img src={logo} className="Header-logo" alt="logo" />
-          SLACKR
-          <span className="Header-divider"></span>
-          {userStatus}
-        </header>
-    );
+  // render() {
+  //   // TODO this code to generate the header should be moved a component.
+  //   var userStatus = <CircularProgress/>; // For loading state
+  //   if(this.state.status === 'in') {
+  //     userStatus = (<div onClick={this.handleOpen.bind(this)}>
+  //       <img alt="user profile"
+  //         className="Header-photo" src={this.state.user.photoURL} />
+  //       <Menu anchorEl={this.state.anchorEl} open={this.state.open}
+  //         onClose={this.handleClose.bind(this)}>
+  //         <MenuItem onClick={this.handleSignOut}>Sign out</MenuItem>
+  //       </Menu>
+  //     </div>);
+  //   } else if(this.state.status === 'out') {
+  //     userStatus = (<Button raised color="primary" onClick={this.handleSignIn}>
+  //       SIGN IN
+  //     </Button>);
+  //   }
+  //   var Header = (
+  //       <header className="Header">
+  //         <img src={logo} className="Header-logo" alt="logo" />
+  //         SLACKR
+  //         <span className="Header-divider"></span>
+  //         {userStatus}
+  //       </header>
+  //   );
 
     // TODO this seems like it could be a component too?
     var channelButtons = [];
@@ -180,19 +180,19 @@ export default class App extends React.Component {
   }
 
   // TODO these functions are only used by the Header
-  handleSignIn() {
-    base.initializedApp.auth()
-      .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
-  }
-  handleSignOut() {
-    base.initializedApp.auth().signOut();
-  }
-  handleOpen(event) {
-    this.setState({ open: true, anchorEl: event.currentTarget });
-  }
-  handleClose() {
-    this.setState({ open: false });
-  }
+  // handleSignIn() {
+  //   base.initializedApp.auth()
+  //     .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  // }
+  // handleSignOut() {
+  //   base.initializedApp.auth().signOut();
+  // }
+  // handleOpen(event) {
+  //   this.setState({ open: true, anchorEl: event.currentTarget });
+  // }
+  // handleClose() {
+  //   this.setState({ open: false });
+  // }
 
   // TODO move these functions to a component where they belong
   setMessagesEnd(el) {
